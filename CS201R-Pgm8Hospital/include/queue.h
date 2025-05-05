996d2f256ba528a38a16c117ba5893812b856655
davisa999 <ashtondavis7999@gmail.com>
2025-05-05 19:32:49 +0000

    Added Queue Code from Class

--- CS201R-Pgm8Hospital/include/queue.h
+++ CS201R-Pgm8Hospital/include/queue.h
@@ -1,1 +1,60 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct dataType {
+	char type;
+	string lname;
+	string fname;
+	int age;
+
+	dataType() : type(' '),lname(""), fname(""),age(0) {}
+};
+
+struct Node {
+	dataType data;
+	Node* nextPtr;
+
+	Node()  {
+		dataType();
+		nextPtr = nullptr;
+	}
+	Node(dataType d) {
+		data = d;
+		nextPtr = nullptr;
+	}
+};
+
+class LinkedList {
+protected:
+	Node* headPtr;
+	Node* tailPtr;
+public:
+	LinkedList();
+	LinkedList(dataType);
+	~LinkedList();  //destructor
+	virtual void addNode(dataType d);
+	void addNodeOrdered(dataType d);
+	virtual void delNode(string name);
+	void printList();
+};
+
+class Queue : public LinkedList {
+public:
+	void delNode() {
+		//condition 1: queue is empty
+		if (headPtr == nullptr) {
+			cout << "the queue is empty" << endl;
+			return;
+		}
+		Node* delNode = headPtr;
+		headPtr = headPtr->nextPtr;
+		if (headPtr == nullptr) {
+			tailPtr = nullptr;
+		}
+		delete delNode;
+		delNode = nullptr;
+	}
+};
