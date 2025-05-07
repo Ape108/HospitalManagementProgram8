cf01031677a5684b2ce42da5f60215bcd273ff85
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2025-05-07 10:27:49 -0500

    HC Constructors

--- CS201R-Pgm8Hospital/src/heart_clinic.cpp
+++ CS201R-Pgm8Hospital/src/heart_clinic.cpp
@@ -1,2 +1,10 @@
 #include "heart_clinic.h"
 
+HeartClinic::HeartClinic() : Clinic() {
+    // Add new class properties
+    
+}
+
+HeartClinic::HeartClinic(string name, Queue& q) : Clinic(name, q) {
+    
+}
--- CS201R-Pgm8Hospital/src/heart_clinic.h
+++ CS201R-Pgm8Hospital/src/heart_clinic.h
@@ -2,3 +2,12 @@
 
 #include "clinic.h"
 
+class HeartClinic : public Clinic {
+public:
+    // Constructors
+    HeartClinic();
+    HeartClinic(string name, Queue& q);
+    
+    // Override Functions
+    
+};
